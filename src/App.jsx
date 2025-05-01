e9046c782ba8e724aa36b6f218b675c65576af6b
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-05-01 08:59:23 +0300

    Add files via upload
    table added

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
-import TopMenuSmart from "./components/TopMenuSmart.jsx";
-import TopMenuView from "./components/TopMenuView.jsx";
+import SidebarSmart from "./components/TopMenu/SidebarSmart.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainTableView from "./components/MainTableView/MainTableView.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <TopMenuSmart />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-3 col-lg-2">
+                    <SidebarSmart />
+                </div>
+                <div className="col-md-9 col-lg-10">
+                    <MainTableView />
+                </div>
+            </div>
+        </div>
     </>
   )
 }
---
+++ src/components/TopMenu/SidebarView.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './SidebarStyles.css';
+
+const SidebarView = ({ initItems, active, handleClickLink }) => {
+    return (
+        <nav className="navbar navbar-light bg-light Sidebar">
+            <div className="container-fluid">
+                {initItems.length > 0 ? (
+                    <div className="navbar-collapse Sidebar-Collapse">
+                        <ul className="navbar-nav Sidebar-List">
+                            {initItems.map((item) => (
+                                <li
+                                    className={`nav-item Sidebar-Item ${active === item.id ? 'Sidebar-Item_Active' : ''}`}
+                                    key={item.id}
+                                    onClick={() => handleClickLink(item)}
+                                >
+                                    <a className="nav-link Sidebar-Link">
+                                        {item.innerText}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                        {/*<DropdownSearchFiltersSmart /> */}
+                    </div>
+                ) : (
+                    {/*<NoDataMessageView /> */}
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default SidebarView;
