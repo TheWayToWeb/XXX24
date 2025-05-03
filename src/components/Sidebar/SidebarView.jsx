d5c9932124b798440779a53567e7f42429e9f036
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-05-03 14:33:13 +0300

    Add files via upload
    Added responsive media queries for sidebar buttons

--- src/components/Sidebar/SidebarView.jsx
+++ src/components/Sidebar/SidebarView.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './SidebarStyles.css';
+import './SidebarButtonsStyles.css';
 
 const SidebarView = ({ initItems, active, handleClickLink }) => {
+    const buttonsData = [
+        {id: 1, label: '+', action: 'add'},
+        {id: 2, label: '-', action: 'subsctract'}
+    ];
     return (
-        <nav className="navbar bg-light Sidebar">
+        <>
+            <nav className="navbar bg-light Sidebar">
                 {initItems.length > 0 ? (
                     <div className="navbar-collapse Sidebar-Collapse">
                         <ul className="navbar-nav Sidebar-List">
@@ -23,7 +29,25 @@ const SidebarView = ({ initItems, active, handleClickLink }) => {
                 ) : (
                     {/*<NoDataMessageView /> */}
                 )}
-        </nav>
+            </nav>
+            <div className="container SidebarButtonsContainer">
+                <div className="row btn-group SidebarButtonItems">
+                    {buttonsData.map((button) => (
+                        <div
+                            className=" col-sm-6 col-md-6 col-lg-6 col-xl-6 SidebarButtonItems-Column"
+                            key={button.id}
+                        >
+                            <button
+                                id={`sidebarButton-${button.id}`}
+                                type="button"
+                                className="btn btn-outline-primary SidebarButtonItems-Button"
+                            >{button.label}</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+
     );
 };
 
