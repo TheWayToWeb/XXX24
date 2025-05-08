be722bccd2cb432681e19fb2b150445789e65e06
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-05-09 01:39:30 +0300

    Add files via upload
    Changed design of drop-down list

--- src/components/Grid/GridView.jsx
+++ src/components/Grid/GridView.jsx
@@ -9,10 +9,10 @@ const GridView = () => {
     return(
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-3 col-lg-2">
+                <div className="col-12 col-sm-4 col-md-3 col-lg-3">
                     <DropdownIndexView />
                 </div>
-                <div className="col-md-9 col-lg-10">
+                <div className="col-12 col-sm-8 col-md-9 col-lg-9">
                     <MainMenuIndexView />
                     <FilterSearcherIndexView
                         searchFields={[
