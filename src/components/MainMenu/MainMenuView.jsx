3a5b0236a62e2e92bdf902161a3ec6bd703e0e59
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-05-09 20:46:10 +0300

    Add files via upload
    Change theme hamberger buttons on color theme microsoft excel

--- src/components/MainMenu/MainMenuView.jsx
+++ src/components/MainMenu/MainMenuView.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef} from 'react';
-import PropTypes from 'prop-types'; // Импортируем PropTypes
+// импортируем библиотеку PropTypes
+import PropTypes from 'prop-types';
+import './BurgerButtonStyles.css';
 import './MainMenuStyles.css';
 
 const MainMenuView = React.memo(({ isActive, onToggleMenu, menuItems }) => {
