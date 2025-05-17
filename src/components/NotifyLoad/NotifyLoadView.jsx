86ce129e2687c2ea94835e094fabe952980ccc18
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-05-17 18:48:12 +0300

    Delete src/components directory

--- src/components/NotifyLoad/NotifyLoadView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotifyLoadView = () => {
-    return (
-        <div className="alert alert-secondary">Загрузка</div>
-    );
-};
-
-export default NotifyLoadView;
--- src/components/TreeDataView/TreeDataView.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TreeDataView = () => {
-    return (
-        <div>В этом месте будет представление дерева данных</div>
-    );
-}
-
-export default TreeDataView;
