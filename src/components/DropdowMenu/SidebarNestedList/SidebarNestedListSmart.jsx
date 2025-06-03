fd988b9cf40fd1a850aec7b2e3d8785f1a19c740
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-06-03 19:51:47 +0300

    Add files via upload
    The functionality of the scrolling menu has been implemented using the ReactList component

--- src/components/DropdowMenu/SidebarNestedList/SidebarNestedListSmart.jsx
+++ src/components/DropdowMenu/SidebarNestedList/SidebarNestedListSmart.jsx
@@ -11,30 +11,36 @@ import {
     QuestionSquare,
     Wrench
 } from "react-bootstrap-icons";
+
+/* Импортируем библиотеку classNames */
+import classNames from "classnames";
 /* Импортируем компонент вложенный список */
 import SidebarNestedListView from './SidebarNestedListView.jsx';
 /* Импорт контекста ButtonStretch */
 import { ButtonStretchContext } from "../DropdownMenuSmart.jsx";
 /* Импорт контекста DropdownList */
 import { DropdownListContext } from "../DropdownMenuList/DropdownMenuListSmart.jsx";
+const classes = classNames(
+    'SidebarIconFontSize'
+);
 /* Преобразуем набор иконок в объект с ключами (маппинг) */
 const iconMapping = {
-    0: <MicrosoftTeams />,
-    1: <Cart3 />,
-    2: <Buildings />,
-    3: <Boxes />,
-    4: <Briefcase />,
-    5: <Book />,
-    6: <CardList />,
-    7: <Wrench />,
-    8: <QuestionSquare />
+    0: <MicrosoftTeams className={classes} />,
+    1: <Cart3 className={classes} />,
+    2: <Buildings className={classes} />,
+    3: <Boxes className={classes} />,
+    4: <Briefcase className={classes} />,
+    5: <Book className={classes} />,
+    6: <CardList className={classes} />,
+    7: <Wrench className={classes} />,
+    8: <QuestionSquare className={classes} />
 };
 /* Блок умного компонента */
 const SidebarItemContainer = () => {
     /* Извлекаем свойство canStretch из ButtonStretchContext */
     const { canStretch } = useContext(ButtonStretchContext);
     /* Аналогично извлекаем свойства из DropdownListContext */
-    const { dropdownListItems, activeIndex, handleActiveClick } = useContext(DropdownListContext);
+    const { dropdownListItems, activeIndex, handleActiveClick, fixedMenuHeight } = useContext(DropdownListContext);
 
     return (
         <SidebarNestedListView
@@ -43,6 +49,7 @@ const SidebarItemContainer = () => {
             items={dropdownListItems}
             activeIndex={activeIndex}
             handleActiveClick={handleActiveClick}
+            fixedMenuHeight={fixedMenuHeight}
         />
     );
 };
--- src/components/DropdowMenu/SidebarNestedList/SidebarNestedListView.jsx
+++ src/components/DropdowMenu/SidebarNestedList/SidebarNestedListView.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 // Импорт библиотеки classNames
 import classNames from "classnames";
+/* Импорт стилей иконки сложенного списка */
+import './SidebarIconStyles.less';
+/* Импорт стилей фрагмента выпадающего списка NestedNodeList */
+import './SidebarNestedNodeListStyles.less';
+// Импорт react-list
+import ReactList from 'react-list';
+
 // Блок описания компонента вложенного списка
 const SidebarNestedListView = ({
-    canStretch,
-    iconMapping,
-    items,
-    activeIndex,
-    handleActiveClick
+   canStretch,
+   iconMapping,
+   items,
+   activeIndex,
+   handleActiveClick,
+   fixedMenuHeight
 }) => {
 
     return (
@@ -17,9 +25,10 @@ const SidebarNestedListView = ({
                     key={item.id}
                     className={classNames(
                         'nav-item',
-                            'Sidebar-Item', {
+                        'Sidebar-Item', {
                             'Sidebar-Item_Active': activeIndex === item.id
-                        })}
+                        }
+                    )}
                     onClick={() => handleActiveClick(item)}
                 >
                     <a className="nav-link Sidebar-Link">
@@ -28,22 +37,35 @@ const SidebarNestedListView = ({
                             <span className="Sidebar-Text">{item.text}</span>
                         )}
                     </a>
-                    <ul>
-                        {item.children.length > 0 && (
-                            item.children.map((child) => (
-                                <li
-                                    className="list-group-item"
-                                    key={child.id}
-                                >
-                                    {child.text}
-                                </li>
-                            ))
+                    <div
+                        className="SidebarNestedNodeList_container"
+                        style={{ overflowY: 'auto', maxHeight: `${fixedMenuHeight}px`}}
+                    >
+                        {/* Проверка наличия детей */}
+                        {item.children && item.children.length > 0 && (
+                            <ReactList
+                                itemRenderer={(index) => {
+                                    const child = item.children[index];
+                                    return (
+                                        <li
+                                            className={classNames(
+                                                'list-group-item',
+                                                'SidebarNestedNodeList-Item'
+                                            )}
+                                            key={child.id}
+                                        >
+                                            {child.text}
+                                        </li>
+                                    );
+                                }}
+                                length={item.children.length}
+                                type='uniform'
+                            />
                         )}
-                    </ul>
+                    </div>
                 </li>
             ))}
         </>
-
     );
 };
 
