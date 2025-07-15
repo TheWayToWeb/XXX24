afdf73e1e568689d3336aa7af415be120d415938
Denis <62526089+TheWayToWeb@users.noreply.github.com>
2025-07-16 01:16:09 +0300

    Delete src/context directory

--- src/context/ProductionContext/ProductionContext.js
+++
@@ -1,5 +0,0 @@
-// Подключаем функцию createContext из библиотеки React для создания контекста
-import { createContext } from 'react';
-
-// Создаем контекст производства
-export const ProductionContext = createContext(null);
--- src/context/StoreContext/StoreContext.js
+++
@@ -1,5 +0,0 @@
-// Подключаем функцию createContext из библиотеки
-import { createContext } from "react";
-
-// Создаем контекст производства
-export const StoreContext = createContext(null);
